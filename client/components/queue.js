55d38a36d90f998c7a6d2f915dcf188e05696953
Georg Donner <georgdonner98@gmail.com>
2018-05-02 11:49:01 +0200

    Improve mobile view.

--- client/components/queue.js
+++ client/components/queue.js
@@ -2,30 +2,48 @@ import { h } from 'hyperapp';
 import convertMs from '../utils/convertMs';
 import '../scss/queue.scss';
 
-const Item = ({ track, user }) => (
-  <tr key={track.id}>
-    <td>{track.name}</td>
-    <td>{track.artists.map(a => a.name).join(', ')}</td>
-    <td>{track.album.name}</td>
-    <td>{convertMs(track.duration_ms)}</td>
-    <td>{user}</td>
-  </tr>
-);
+const Item = ({ track, user, mobile }) => {
+  const artists = track.artists.map(a => a.name).join(', ');
+  return mobile ? (
+    <tr key={track.id} class="track-mobile">
+      <td>
+        <div class="track-name-mobile">{track.name}</div>
+        <div class="track-info-mobile">
+          {artists}
+          <span class="delimiter-mobile">•</span>
+          {track.album.name}
+        </div>
+      </td>
+    </tr>
+  ) : (
+    <tr key={track.id}>
+      <td>{track.name}</td>
+      <td>{track.artists.map(a => a.name).join(', ')}</td>
+      <td>{track.album.name}</td>
+      <td>{convertMs(track.duration_ms)}</td>
+      <td>{user}</td>
+    </tr>
+  );
+};
 
 export default () => (state) => {
+  const mobile = window.matchMedia('(max-width: 1000px)').matches;
+  const head = !mobile ? (
+    <tr>
+      <th>Name</th>
+      <th>Artists</th>
+      <th>Album</th>
+      <th>Duration</th>
+      <th>Added by</th>
+    </tr>
+  ) : null;
   const tracks = state.queue.map(item => (
-    <Item track={item.track} user={item.user} />
+    <Item track={item.track} user={item.user} mobile={mobile} />
   ));
   return (
     <div class="queue">
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Artists</th>
-          <th>Album</th>
-          <th>Duration</th>
-          <th>Added by</th>
-        </tr>
+        {head}
         {tracks}
       </table>
     </div>
--- client/components/sortableQueue.js
+++ client/components/sortableQueue.js
@@ -4,23 +4,51 @@ import IconButton from './iconButton';
 import convertMs from '../utils/convertMs';
 import '../scss/queue.scss';
 
-const Item = ({ track, onRemove }) => (
-  <tr key={track.id} class="item" data-id={track.id}>
-    <td>{track.name}</td>
-    <td>{track.artists.map(a => a.name).join(', ')}</td>
-    <td>{track.album.name}</td>
-    <td>{convertMs(track.duration_ms)}</td>
-    <td>
-      <IconButton
-        classes="remove fas fa-trash"
-        onClick={() => onRemove(track.id)}
-      />
-    </td>
-  </tr>
-);
+const Item = ({ track, onRemove, mobile }) => {
+  const artists = track.artists.map(a => a.name).join(', ');
+  const removeButton = (
+    <IconButton
+      classes="remove fas fa-trash"
+      onClick={() => onRemove(track.id)}
+    />
+  );
+  return mobile ? (
+    <tr key={track.id} class="item track-mobile" data-id={track.id}>
+      <td>
+        <div class="track-name-mobile">{track.name}</div>
+        <div class="track-info-mobile">
+          {artists}
+          <span class="delimiter-mobile">•</span>
+          {track.album.name}
+        </div>
+      </td>
+      <td>{removeButton}</td>
+    </tr>
+  ) : (
+    <tr key={track.id} class="item" data-id={track.id}>
+      <td>{track.name}</td>
+      <td>{artists}</td>
+      <td>{track.album.name}</td>
+      <td>{convertMs(track.duration_ms)}</td>
+      <td>{removeButton}</td>
+    </tr>
+  );
+};
 
 export default ({ user, onReorder, onRemove }) => () => {
-  const tracks = user.queue.map(track => <Item track={track} onRemove={onRemove} />);
+  const mobile = window.matchMedia('(max-width: 1000px)').matches;
+  const head = !mobile ? (
+    <tr>
+      <th>Name</th>
+      <th>Artists</th>
+      <th>Album</th>
+      <th>Duration</th>
+      <th />
+    </tr>
+  ) : null;
+  const tracks = user.queue.map(track => (
+    <Item track={track} onRemove={onRemove} mobile={mobile} />
+  ));
 
   return (
     <div class="queue">
@@ -36,13 +64,7 @@ export default ({ user, onReorder, onRemove }) => () => {
         }}
       >
         <table id="queue-sortable">
-          <tr>
-            <th>Name</th>
-            <th>Artists</th>
-            <th>Album</th>
-            <th>Duration</th>
-            <th />
-          </tr>
+          {head}
           {tracks}
         </table>
       </div>
