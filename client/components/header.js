c18a21a715c882468ed94f10ad65afa998607f94
Jonathan <joni.derin@gmail.com>
2018-07-09 22:18:19 +0200

    create more spotify-like ui

---
+++ client/components/header.js
@@ -0,0 +1,11 @@
+import { h } from 'hyperapp';
+import '../scss/header.scss';
+
+export default () => {
+  return (
+    <div id="header">
+        <h1>Jukebox</h1>
+        <h2>queue system for your spotify party</h2>
+    </div>
+  );
+};
